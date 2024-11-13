658edd5c7b055c52915fef042786852dbedab800
emilio-EETech <emilio.morales@eetechnologiesllc.com>
2024-11-12 20:43:30 -0500

    Finalized top row of bento grid for metrics section

--- src/components/home/users/BentoGrid3.jsx
+++ src/components/home/users/BentoGrid3.jsx
@@ -5,9 +5,6 @@ export default async function BentoGrid3() {
   const usersJoined2024 = await getUsersJoinedByMonth(2024)
   const usersLeft2024 = await getUsersLeftByMonth(2024)
 
-  console.log('joined:', usersJoined2024)
-  console.log('left:', usersLeft2024)
-
   const totalJoined2024 = usersJoined2024.totalEmployeesJoined
   const totalLeft2024 = usersLeft2024.totalEmployeesLeft
   const totalEmployees2024 = usersJoined2024.totalEmployees
@@ -15,23 +12,20 @@ export default async function BentoGrid3() {
   const joinedPercentage2024 = ((totalJoined2024 / totalEmployees2024) * 100).toFixed(2)
   const exitedPercentage2024 = ((totalLeft2024 / totalEmployees2024) * 100).toFixed(2)
 
+  const usersJoined2023 = await getUsersJoinedByMonth(2023)
+  const usersLeft2023 = await getUsersLeftByMonth(2023)
+
+  const totalJoined2023 = usersJoined2023.totalEmployeesJoined
+  const totalLeft2023 = usersLeft2023.totalEmployeesLeft
+  const totalEmployees2023 = usersJoined2023.totalEmployees
+
+  const joinedPercentage2023 = ((totalJoined2023 / totalEmployees2023) * 100).toFixed(2)
+  const exitedPercentage2023 = ((totalLeft2023 / totalEmployees2023) * 100).toFixed(2)
+
   return (
-    <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
-      <div className="relative lg:col-span-3 xl:col-span-2">
-        <div className="absolute inset-px rounded-lg border border-zinc-950/5 dark:border-white/10" />
+    <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-12">
+      <div className="relative lg:col-span-6 xl:col-span-3 xl:w-full xl:max-w-72">
         <div className="relative flex h-fit flex-col overflow-hidden">
-          {/* <img
-            alt=""
-            src="https://tailwindui.com/plus/img/component-images/bento-01-speed.png"
-            className="h-80 object-cover object-left"
-            /> */}
-          {/* <div className="p-10 pt-4">
-            <h3 className="text-sm/4 font-semibold text-indigo-600">Speed</h3>
-            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Built for power users</p>
-            <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-              Sed congue eros non finibus molestie. Vestibulum euismod augue.
-            </p>
-          </div> */}
           {/* title, value, interval, trend, data */}
           <StatCard
             title="Employees Joined"
@@ -43,16 +37,9 @@ export default async function BentoGrid3() {
           />
           {/* <EmployeesJoinedLineGraph /> */}
         </div>
-        {/* <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-bl-[2rem]" /> */}
       </div>
-      <div className="relative lg:col-span-3 xl:col-span-2">
-        <div className="absolute inset-px rounded-lg border border-zinc-950/5 dark:border-white/10" />
+      <div className="relative lg:col-span-6 xl:col-span-3 xl:w-full xl:max-w-72">
         <div className="relative flex h-fit flex-col overflow-hidden">
-          {/* <img
-            alt=""
-            src="https://tailwindui.com/plus/img/component-images/bento-01-integrations.png"
-            className="h-80 object-cover object-center"
-          /> */}
           <StatCard
             title="Employees Exited"
             value={totalLeft2024}
@@ -61,17 +48,33 @@ export default async function BentoGrid3() {
             data={usersLeft2024.monthlyData}
             rate={exitedPercentage2024}
           />
-          {/* <div className="p-10 pt-4">
-            <h3 className="text-sm/4 font-semibold text-indigo-600">Integrations</h3>
-            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950">Connect your favorite tools</p>
-            <p className="mt-2 max-w-lg text-sm/6 text-gray-600">
-              Maecenas at augue sed elit dictum vulputate, in nisi aliquam maximus arcu.
-            </p>
-          </div> */}
         </div>
-        {/* <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" /> */}
       </div>
-      <div className="relative lg:col-span-3 xl:col-span-2">
+      <div className="relative lg:col-span-6 xl:col-span-3 xl:w-full xl:max-w-72">
+        <div className="relative flex h-fit flex-col overflow-hidden">
+          <StatCard
+            title="Employees Joined"
+            value={totalJoined2023}
+            interval="2023"
+            trend="up"
+            data={usersJoined2023.monthlyData}
+            rate={joinedPercentage2023}
+          />
+        </div>
+      </div>
+      <div className="relative lg:col-span-6 xl:col-span-3 xl:w-full xl:max-w-72">
+        <div className="relative flex h-fit flex-col overflow-hidden">
+          <StatCard
+            title="Employees Exited"
+            value={totalLeft2023}
+            interval="2023"
+            trend="down"
+            data={usersLeft2023.monthlyData}
+            rate={exitedPercentage2023}
+          />
+        </div>
+      </div>
+      <div className="relative lg:col-span-6 xl:col-span-6">
         <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)]">
           <img
@@ -89,7 +92,7 @@ export default async function BentoGrid3() {
         </div>
         <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-br-[2rem]" />
       </div>
-      <div className="relative lg:col-span-3">
+      <div className="relative lg:col-span-6">
         <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]" />
         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-tl-[calc(2rem+1px)]">
           <img
@@ -108,7 +111,7 @@ export default async function BentoGrid3() {
         </div>
         <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem] lg:rounded-tl-[2rem]" />
       </div>
-      <div className="relative lg:col-span-3">
+      <div className="relative lg:col-span-6">
         <div className="absolute inset-px rounded-lg bg-white lg:rounded-tr-[2rem]" />
         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-tr-[calc(2rem+1px)]">
           <img
