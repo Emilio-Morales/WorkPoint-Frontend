95d3e5bd270690b475af75595fdcf2197ae28cec
emilio-EETech <emilio.morales@eetechnologiesllc.com>
2025-01-02 16:30:33 -0500

    Fixed import for button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 
@@ -148,7 +149,7 @@ export default function HomePage() {
           </div>
         </div>
       ) : (
-        <>Server only</>
+        <>Loading...</>
       )}
     </>
   )
